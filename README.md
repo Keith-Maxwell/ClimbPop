3d123857b5eeb951128acc76f493f5228c1ec422
Max Perraudin <max.perraudin@gmail.com>
2020-07-03 23:02:10 +0200

    Add message over progress bar. Remove Massy

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ This small python program gathers from Arkose Websites the current capacity of t
 - [Arkose Massy](https://massy.arkose.com/)
 
 I used the requests module as well as BeautifulSoup.
+
+## Update : 03/07/2020
+
+Arkose Massy removed it’s counter because there never was a real need for it, the gym is never full (it’s so huge lol).
--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+from typing import Tuple
 
 
-def printProgressBar(gymName: str, percent: int, length: int = 100):
+def printProgressBar(gymName: str, percent: int, bonus_text: str, length: int = 100):
     filled = '█' * (percent * length // 100)
     empty = '-' * (length - len(filled))
-    print(f'{gymName} : {percent} %')
+    print(f'\n{gymName} : {percent} %\t|\t{bonus_text}')
     print(f'|{filled}{empty}|\n')
 
 
-def getFillPercent(URL: str) -> int:
+def getFillPercent(URL: str) -> Tuple[int, str]:
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
 
     tag = soup.find_all("progress")[0]
     percent = tag['value']
-    return int(percent)
+    jauge = soup.find_all('div', class_='jauge')[0]
+    text = jauge.find_all('span')[-1].text
+    return int(percent), text
 
 
 URL_Montreuil = "https://montreuil.arkose.com/"
 URL_Nation = "https://nation.arkose.com/"
-URL_Massy = "https://massy.arkose.com/"
+# URL_Massy = "https://massy.arkose.com/"
 
-printProgressBar("Arkose Montreuil", getFillPercent(URL_Montreuil))
-printProgressBar("Arkose Nation", getFillPercent(URL_Nation))
-printProgressBar("Arkose Massy", getFillPercent(URL_Massy))
+p, t = getFillPercent(URL_Montreuil)
+printProgressBar("Arkose Montreuil", p, t)
+p, t = getFillPercent(URL_Nation)
+printProgressBar("Arkose Nation", p, t)
+# p, t = getFillPercent(URL_Massy)
+# printProgressBar("Arkose Massy", p, t)
